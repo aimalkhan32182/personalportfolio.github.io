599b1ef1cbed62faf6c9a2d9129e4b0c8dc1a75b
aimalkhan32182 <75561513+aimalkhan32182@users.noreply.github.com>
2022-07-09 11:03:41 +0500

    Personal Portfolio website

---
+++ js/script.js
@@ -0,0 +1,6 @@
+var typed = new Typed(".typing", {
+    strings:[",","Fultter Developer","Web Developer","UI Designer","Back-end Developer"],
+    typeSpeed:90,
+    BackSpeed:60,
+    loop:true
+})
